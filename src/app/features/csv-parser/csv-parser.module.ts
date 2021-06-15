e5691d0b2a540e7c53b3f1e0f11148e9b2034237
Wataru.Kasahara <kasahara1019@gmail.com>
2021-06-15 19:55:40 +0900

    yarn ng g c features/csv-parser/pages/csv-parser

--- src/app/features/csv-parser/csv-parser.module.ts
+++ src/app/features/csv-parser/csv-parser.module.ts
@@ -2,9 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { CsvParserRoutingModule } from './csv-parser-routing.module';
+import { CsvParserComponent } from './pages/csv-parser/csv-parser.component';
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    CsvParserComponent
+  ],
   imports: [CommonModule, CsvParserRoutingModule],
 })
 export class CsvParserModule {}
