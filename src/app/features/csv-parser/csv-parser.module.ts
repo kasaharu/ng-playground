ccf6e336eddf1becbe65b7a7f7ae7ce2ac130365
Wataru.Kasahara <kasahara1019@gmail.com>
2021-06-15 19:52:15 +0900

    yarn ng g module features/csv-parser --routing

---
+++ src/app/features/csv-parser/csv-parser.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { CsvParserRoutingModule } from './csv-parser-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    CsvParserRoutingModule
+  ]
+})
+export class CsvParserModule { }
