89e8e7bad0cfd3d6ce4efd8a808858ef48903726
Wataru.Kasahara <kasahara1019@gmail.com>
2021-06-18 21:17:22 +0900

    yarn format

--- src/app/features/csv-parser/containers/csv-parser/csv-parser.component.spec.ts
+++ src/app/features/csv-parser/containers/csv-parser/csv-parser.component.spec.ts
@@ -8,9 +8,8 @@ describe('CsvParserComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ CsvParserComponent ]
-    })
-    .compileComponents();
+      declarations: [CsvParserComponent],
+    }).compileComponents();
   });
 
   beforeEach(() => {
--- src/app/features/csv-parser/containers/csv-parser/csv-parser.component.ts
+++ src/app/features/csv-parser/containers/csv-parser/csv-parser.component.ts
@@ -3,13 +3,10 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-csv-parser',
   templateUrl: './csv-parser.component.html',
-  styleUrls: ['./csv-parser.component.scss']
+  styleUrls: ['./csv-parser.component.scss'],
 })
 export class CsvParserComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
