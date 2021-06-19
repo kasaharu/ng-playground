3e9a9ef220acccf37c398f354c9a91639bf87f62
Wataru.Kasahara <kasahara1019@gmail.com>
2021-06-19 20:27:59 +0900

    fix: Page container から selector を削除

--- src/app/features/csv-parser/pages/csv-parser/csv-parser.component.ts
+++ src/app/features/csv-parser/pages/csv-parser/csv-parser.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-csv-parser',
   templateUrl: './csv-parser.component.html',
   styleUrls: ['./csv-parser.component.scss'],
 })
