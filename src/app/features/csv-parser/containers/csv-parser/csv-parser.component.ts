079a0c563ee4b203854460c70a075cc643353da0
Wataru.Kasahara <kasahara1019@gmail.com>
2021-06-22 21:33:48 +0900

    feat: CSV のパース機能を実装

--- src/app/features/csv-parser/containers/csv-parser/csv-parser.component.ts
+++ src/app/features/csv-parser/containers/csv-parser/csv-parser.component.ts
@@ -1,12 +1,29 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-csv-parser',
   templateUrl: './csv-parser.component.html',
   styleUrls: ['./csv-parser.component.scss'],
 })
-export class CsvParserComponent implements OnInit {
-  constructor() {}
+export class CsvParserComponent {
+  @ViewChild('csvUpload') csvUploadElement!: ElementRef<HTMLInputElement>;
 
-  ngOnInit(): void {}
+  parse(): void {
+    const files: FileList | null = this.csvUploadElement.nativeElement.files;
+    if (files === null || files.length === 0) {
+      return;
+    }
+
+    const file: File | null = files.item(0);
+    if (file === null) {
+      return;
+    }
+
+    const reader: FileReader = new FileReader();
+    reader.readAsText(file);
+    reader.onload = () => {
+      let csv = reader.result as string;
+      console.log(csv);
+    };
+  }
 }
