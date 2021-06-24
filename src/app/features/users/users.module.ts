1746dca2470b1a9a059934d0b715dfa7e17b0029
Wataru.Kasahara <kasahara1019@gmail.com>
2021-06-24 21:52:55 +0900

    yarn ng g c features/users/containers/users

--- src/app/features/users/users.module.ts
+++ src/app/features/users/users.module.ts
@@ -3,9 +3,10 @@ import { CommonModule } from '@angular/common';
 
 import { UsersRoutingModule } from './users-routing.module';
 import { UsersPageComponent } from './pages/users/users.component';
+import { UsersComponent } from './containers/users/users.component';
 
 @NgModule({
-  declarations: [UsersPageComponent],
+  declarations: [UsersPageComponent, UsersComponent],
   imports: [CommonModule, UsersRoutingModule],
 })
 export class UsersModule {}
