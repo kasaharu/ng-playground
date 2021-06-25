92f0b53589577e230f5ebe7b793d946d4fb4ee90
Wataru.Kasahara <kasahara1019@gmail.com>
2021-06-25 21:42:39 +0900

    refactor: page componet から不要な lifecycle method 削除

--- src/app/features/users/pages/users/users.component.ts
+++ src/app/features/users/pages/users/users.component.ts
@@ -1,10 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   template: `<app-users></app-users>`,
 })
-export class UsersPageComponent implements OnInit {
-  constructor() {}
-
-  ngOnInit(): void {}
-}
+export class UsersPageComponent {}
