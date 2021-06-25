821d3a12b248b89e85ae01a3ef4a4694e9eeb6d7
Wataru.Kasahara <kasahara1019@gmail.com>
2021-06-25 21:32:55 +0900

    feat: /users のルーティング定義

--- src/app/features/users/users-routing.module.ts
+++ src/app/features/users/users-routing.module.ts
@@ -1,7 +1,8 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { UsersPageComponent } from './pages/users/users.component';
 
-const routes: Routes = [];
+const routes: Routes = [{ path: '', component: UsersPageComponent, pathMatch: 'full' }];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
