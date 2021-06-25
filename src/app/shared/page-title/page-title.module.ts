c7ee6d540e6f9487c9a839d01d4e59a6ff7f6668
Wataru.Kasahara <kasahara1019@gmail.com>
2021-06-25 09:16:35 +0900

    yarn format

--- src/app/shared/page-title/page-title.module.ts
+++ src/app/shared/page-title/page-title.module.ts
@@ -2,14 +2,8 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { PageTitleComponent } from './views/page-title/page-title.component';
 
-
-
 @NgModule({
-  declarations: [
-    PageTitleComponent
-  ],
-  imports: [
-    CommonModule
-  ]
+  declarations: [PageTitleComponent],
+  imports: [CommonModule],
 })
-export class PageTitleModule { }
+export class PageTitleModule {}
--- src/app/shared/page-title/views/page-title/page-title.component.ts
+++ src/app/shared/page-title/views/page-title/page-title.component.ts
@@ -3,13 +3,10 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-page-title',
   templateUrl: './page-title.component.html',
-  styleUrls: ['./page-title.component.scss']
+  styleUrls: ['./page-title.component.scss'],
 })
 export class PageTitleComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
