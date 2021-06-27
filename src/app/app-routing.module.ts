0fb94066d6aa18efcde0db87db9d4328585471ac
Wataru.Kasahara <kasahara1019@gmail.com>
2021-06-27 17:35:13 +0900

    feat: デフォルトページを /users に設定

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
+  { path: '', redirectTo: 'users', pathMatch: 'full' },
   { path: 'users', loadChildren: () => import('./features/users/users.module').then((m) => m.UsersModule) },
   { path: 'csv-parser', loadChildren: () => import('./features/csv-parser/csv-parser.module').then((m) => m.CsvParserModule) },
 ];
