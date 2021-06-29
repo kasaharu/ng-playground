71acefa70234772f6d322a2cd1711a87004825d6
Wataru.Kasahara <kasahara1019@gmail.com>
2021-06-29 21:27:46 +0900

    feat: User を取得する api class を実装

--- src/app/infrastructures/api/user.service.spec.ts
+++ src/app/infrastructures/api/user.service.spec.ts
@@ -1,15 +1,54 @@
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 import { TestBed } from '@angular/core/testing';
+import { User } from '../../domain/user';
 import { UserApi } from './user.service';
 
 describe('UserApi', () => {
   let api: UserApi;
+  let httpTestingController: HttpTestingController;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+    });
     api = TestBed.inject(UserApi);
+    httpTestingController = TestBed.inject(HttpTestingController);
   });
 
   it('should be created', () => {
     expect(api).toBeTruthy();
+  });
+
+  afterEach(() => {
+    httpTestingController.verify();
+  });
+
+  it('getUsers()', () => {
+    const expected: User[] = [
+      {
+        id: 1,
+        name: 'test',
+        username: 'sample',
+        email: '',
+        address: {
+          street: '',
+          suite: '',
+          city: '',
+          zipcode: '',
+          geo: { lat: '', lng: '' },
+        },
+        phone: '',
+        website: '',
+        company: { name: '', catchPhrase: '', bs: '' },
+      },
+    ];
+
+    api.getUsers().subscribe((resp) => {
+      expect(resp).toEqual(expected);
+    });
+
+    const req = httpTestingController.expectOne('https://jsonplaceholder.typicode.com/users');
+    expect(req.request.method).toEqual('GET');
+    req.flush(expected);
   });
 });
--- src/app/infrastructures/api/user.service.ts
+++ src/app/infrastructures/api/user.service.ts
@@ -1,8 +1,15 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { User } from '../../domain/user';
 
 @Injectable({
   providedIn: 'root',
 })
 export class UserApi {
-  constructor() {}
+  constructor(private readonly _http: HttpClient) {}
+
+  getUsers(): Observable<User> {
+    return this._http.get<User>('https://jsonplaceholder.typicode.com/users');
+  }
 }
