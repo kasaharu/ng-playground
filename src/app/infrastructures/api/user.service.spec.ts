41d0c10d6b6ff0b24500354856061b22fc8191db
Wataru.Kasahara <kasahara1019@gmail.com>
2021-06-29 20:55:25 +0900

    refactor: Rename UserService -> UserApi

--- src/app/infrastructures/api/user.service.spec.ts
+++ src/app/infrastructures/api/user.service.spec.ts
@@ -1,16 +1,15 @@
 import { TestBed } from '@angular/core/testing';
+import { UserApi } from './user.service';
 
-import { UserService } from './user.service';
-
-describe('UserService', () => {
-  let service: UserService;
+describe('UserApi', () => {
+  let api: UserApi;
 
   beforeEach(() => {
     TestBed.configureTestingModule({});
-    service = TestBed.inject(UserService);
+    api = TestBed.inject(UserApi);
   });
 
   it('should be created', () => {
-    expect(service).toBeTruthy();
+    expect(api).toBeTruthy();
   });
 });
