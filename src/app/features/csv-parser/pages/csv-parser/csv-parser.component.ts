04c94dc33ccacaee5c331c55fac80e53967d0af4
Wataru.Kasahara <kasahara1019@gmail.com>
2021-07-25 14:49:17 +0900

    fix: 未使用の lifecycle method を削除

--- src/app/features/csv-parser/pages/csv-parser/csv-parser.component.ts
+++ src/app/features/csv-parser/pages/csv-parser/csv-parser.component.ts
@@ -1,11 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   templateUrl: './csv-parser.component.html',
   styleUrls: ['./csv-parser.component.scss'],
 })
-export class CsvParserPageComponent implements OnInit {
-  constructor() {}
-
-  ngOnInit(): void {}
-}
+export class CsvParserPageComponent {}
--- src/app/features/users/views/user-list/user-list.component.ts
+++ src/app/features/users/views/user-list/user-list.component.ts
@@ -1,12 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-user-list',
   templateUrl: './user-list.component.html',
   styleUrls: ['./user-list.component.scss'],
 })
-export class UserListComponent implements OnInit {
-  constructor() {}
-
-  ngOnInit(): void {}
-}
+export class UserListComponent {}
