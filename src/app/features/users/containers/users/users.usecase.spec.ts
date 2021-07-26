0acbd8c6ef6787345b461945dac4100f4c51c4e6
Wataru.Kasahara <kasahara1019@gmail.com>
2021-07-26 20:48:04 +0900

    feat: usecase で user の取得と保存ができるように実装

--- src/app/features/users/containers/users/users.usecase.spec.ts
+++ src/app/features/users/containers/users/users.usecase.spec.ts
@@ -1,12 +1,17 @@
 import { TestBed } from '@angular/core/testing';
+import { UserApi } from '../../../../infrastructures/api/user.api';
 import { UsersUsecase } from './users.usecase';
+
+class MockUserApi {
+  getUsers(): void {}
+}
 
 describe('UsersUsecase', () => {
   let usecase: UsersUsecase;
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-      providers: [UsersUsecase],
+      providers: [UsersUsecase, { provide: UserApi, useClass: MockUserApi }],
     });
     usecase = TestBed.inject(UsersUsecase);
   });
--- src/app/features/users/containers/users/users.usecase.ts
+++ src/app/features/users/containers/users/users.usecase.ts
@@ -1,6 +1,8 @@
 import { Injectable } from '@angular/core';
 import { ComponentStore } from '@ngrx/component-store';
+import { Observable } from 'rxjs';
 import { User } from '../../../../domain/user';
+import { UserApi } from '../../../../infrastructures/api/user.api';
 
 export interface UserStore {
   users: User[] | null;
@@ -8,7 +10,15 @@ export interface UserStore {
 
 @Injectable()
 export class UsersUsecase extends ComponentStore<UserStore> {
-  constructor() {
+  constructor(private readonly _userApi: UserApi) {
     super({ users: null });
+  }
+
+  readonly users$: Observable<User[] | null> = this.select((state) => state.users);
+  readonly saveUsers = this.updater((_, users: User[]) => ({ users }));
+
+  async fetchUsers(): Promise<void> {
+    const users = await this._userApi.getUsers().toPromise();
+    this.saveUsers(users);
   }
 }
