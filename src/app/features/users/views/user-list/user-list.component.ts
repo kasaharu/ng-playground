ba43665068665cb2604ef1e494339cdf2ccc00ca
Wataru.Kasahara <kasahara1019@gmail.com>
2021-07-28 09:13:01 +0900

    feat: UserListComponent が users を受け取れるようにした

--- src/app/features/users/views/user-list/user-list.component.ts
+++ src/app/features/users/views/user-list/user-list.component.ts
@@ -1,8 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { User } from '../../../../domain/user';
 
 @Component({
   selector: 'app-user-list',
   templateUrl: './user-list.component.html',
   styleUrls: ['./user-list.component.scss'],
 })
-export class UserListComponent {}
+export class UserListComponent {
+  @Input() users: User[] | null = null;
+}
