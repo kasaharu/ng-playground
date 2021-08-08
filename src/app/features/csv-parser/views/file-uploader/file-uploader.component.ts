ff6eb4e20f07be781438c906b07775008f1cc516
Wataru.Kasahara <kasahara1019@gmail.com>
2021-08-08 14:03:36 +0900

    refactor(csv-parser): 未使用な lifecyle method を削除

--- src/app/features/csv-parser/views/file-uploader/file-uploader.component.ts
+++ src/app/features/csv-parser/views/file-uploader/file-uploader.component.ts
@@ -1,12 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-file-uploader',
   templateUrl: './file-uploader.component.html',
   styleUrls: ['./file-uploader.component.scss'],
 })
-export class FileUploaderComponent implements OnInit {
-  constructor() {}
-
-  ngOnInit(): void {}
-}
+export class FileUploaderComponent {}
