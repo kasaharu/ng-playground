be0594f29bbddfe0046edbc230a0dbf36d88296e
Wataru.Kasahara <kasahara1019@gmail.com>
2021-08-08 13:57:37 +0900

    yarn ng g c features/csv-parser/views/file-uploader

--- src/app/features/csv-parser/csv-parser.module.ts
+++ src/app/features/csv-parser/csv-parser.module.ts
@@ -4,9 +4,10 @@ import { CommonModule } from '@angular/common';
 import { CsvParserRoutingModule } from './csv-parser-routing.module';
 import { CsvParserPageComponent } from './pages/csv-parser/csv-parser.component';
 import { CsvParserComponent } from './containers/csv-parser/csv-parser.component';
+import { FileUploaderComponent } from './views/file-uploader/file-uploader.component';
 
 @NgModule({
-  declarations: [CsvParserPageComponent, CsvParserComponent],
+  declarations: [CsvParserPageComponent, CsvParserComponent, FileUploaderComponent],
   imports: [CommonModule, CsvParserRoutingModule],
 })
 export class CsvParserModule {}
