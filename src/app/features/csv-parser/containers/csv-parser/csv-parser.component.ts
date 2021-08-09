0a16624d51ce9fa1ddfc621099e41b81d464a5ff
Wataru.Kasahara <kasahara1019@gmail.com>
2021-08-09 10:39:42 +0900

    feat(csv-parser): ファイルアップロード部分の view を FileUploaderComponent に委譲

--- src/app/features/csv-parser/containers/csv-parser/csv-parser.component.ts
+++ src/app/features/csv-parser/containers/csv-parser/csv-parser.component.ts
@@ -1,7 +1,7 @@
-import { Component, ElementRef, ViewChild } from '@angular/core';
+import { Component } from '@angular/core';
 import { BehaviorSubject } from 'rxjs';
 
-const CSV_MIME_TYPE = 'text/csv';
+// const CSV_MIME_TYPE = 'text/csv';
 
 @Component({
   selector: 'app-csv-parser',
@@ -9,61 +9,32 @@ const CSV_MIME_TYPE = 'text/csv';
   styleUrls: ['./csv-parser.component.scss'],
 })
 export class CsvParserComponent {
-  @ViewChild('csvUpload', { static: true }) csvUploadElement!: ElementRef<HTMLInputElement>;
-
   isAttachedInvalidFile$ = new BehaviorSubject<boolean>(false);
   isParsable = false;
 
   isAttachedCsvFile(): boolean {
-    const files: FileList | null = this.csvUploadElement.nativeElement.files;
-    if (files !== null && files[0].type === CSV_MIME_TYPE) {
-      return true;
-    }
+    // const files: FileList | null = this.csvUploadElement.nativeElement.files;
+    // if (files !== null && files[0].type === CSV_MIME_TYPE) {
+    //   return true;
+    // }
 
     return false;
   }
 
-  changeInputElement(): void {
-    this.isParsable = this.isAttachedCsvFile();
-    this.isAttachedInvalidFile$.next(!this.isParsable);
-  }
-
   parse(): void {
-    const files: FileList | null = this.csvUploadElement.nativeElement.files;
-    if (files === null || files.length === 0) {
-      return;
-    }
-
-    const file: File | null = files.item(0);
-    if (file === null) {
-      return;
-    }
-
-    const reader: FileReader = new FileReader();
-    reader.readAsText(file);
-    reader.onload = () => {
-      let csv = reader.result as string;
-      console.log(csv);
-    };
-  }
-
-  dragOver(event: DragEvent) {
-    event.preventDefault();
-  }
-
-  drop(event: DragEvent) {
-    event.preventDefault();
-    const files: FileList | null = event.dataTransfer ? event.dataTransfer.files : null;
-
-    if (files === null || files.length === 0) {
-      return;
-    }
-    this.csvUploadElement.nativeElement.files = files;
-    this.isParsable = this.isAttachedCsvFile();
-    this.isAttachedInvalidFile$.next(!this.isParsable);
-  }
-
-  dragLeave(event: DragEvent) {
-    console.log('dragLeave', event);
+    // const files: FileList | null = this.csvUploadElement.nativeElement.files;
+    // if (files === null || files.length === 0) {
+    //   return;
+    // }
+    // const file: File | null = files.item(0);
+    // if (file === null) {
+    //   return;
+    // }
+    // const reader: FileReader = new FileReader();
+    // reader.readAsText(file);
+    // reader.onload = () => {
+    //   let csv = reader.result as string;
+    //   console.log(csv);
+    // };
   }
 }
--- src/app/features/csv-parser/views/file-uploader/file-uploader.component.ts
+++ src/app/features/csv-parser/views/file-uploader/file-uploader.component.ts
@@ -1,8 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-file-uploader',
   templateUrl: './file-uploader.component.html',
   styleUrls: ['./file-uploader.component.scss'],
 })
-export class FileUploaderComponent {}
+export class FileUploaderComponent {
+  @ViewChild('csvUpload', { static: true }) csvUploadElement!: ElementRef<HTMLInputElement>;
+
+  changeInputElement(): void {}
+
+  dragOver(event: DragEvent) {
+    event.preventDefault();
+  }
+
+  drop(event: DragEvent) {
+    event.preventDefault();
+    const files: FileList | null = event.dataTransfer ? event.dataTransfer.files : null;
+
+    if (files === null || files.length === 0) {
+      return;
+    }
+    this.csvUploadElement.nativeElement.files = files;
+  }
+
+  dragLeave(event: DragEvent) {
+    console.log('dragLeave', event);
+  }
+}
