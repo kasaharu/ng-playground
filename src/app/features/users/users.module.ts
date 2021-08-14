c9c55fcbfcbf6f35688076112ed063f0669aa51f
Wataru.Kasahara <kasahara1019@gmail.com>
2021-08-14 15:00:09 +0900

    feat(users): user 情報を編集するためのフォームを作成

--- src/app/features/users/users.module.ts
+++ src/app/features/users/users.module.ts
@@ -1,16 +1,16 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
-import { UsersRoutingModule } from './users-routing.module';
-import { UsersPageComponent } from './pages/users/users.component';
+import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
+import { PageTitleModule } from '../../shared/page-title/page-title.module';
 import { UsersComponent } from './containers/users/users.component';
-import { UserListComponent } from './views/user-list/user-list.component';
-import { PageTitleModule } from 'src/app/shared/page-title/page-title.module';
-import { UserListEditorComponent } from './views/user-list-editor/user-list-editor.component';
+import { UsersPageComponent } from './pages/users/users.component';
+import { UsersRoutingModule } from './users-routing.module';
 import { UserEditorFormComponent } from './views/user-editor-form/user-editor-form.component';
+import { UserListEditorComponent } from './views/user-list-editor/user-list-editor.component';
+import { UserListComponent } from './views/user-list/user-list.component';
 
 @NgModule({
   declarations: [UsersPageComponent, UsersComponent, UserListComponent, UserListEditorComponent, UserEditorFormComponent],
-  imports: [CommonModule, UsersRoutingModule, PageTitleModule],
+  imports: [CommonModule, ReactiveFormsModule, UsersRoutingModule, PageTitleModule],
 })
 export class UsersModule {}
--- src/app/features/users/views/user-editor-form/user-editor-form.component.ts
+++ src/app/features/users/views/user-editor-form/user-editor-form.component.ts
@@ -1,5 +1,6 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { User } from '../../../../domain/user';
+import { FormGroup, FormControl } from '@ngneat/reactive-forms';
 
 @Component({
   selector: 'app-user-editor-form',
@@ -8,7 +9,40 @@ import { User } from '../../../../domain/user';
 })
 export class UserEditorFormComponent implements OnInit {
   constructor() {}
-  @Input() user: User | null = null;
+  @Input() user!: User;
 
-  ngOnInit(): void {}
+  form = new FormGroup<User>({
+    id: new FormControl(),
+    name: new FormControl(''),
+    username: new FormControl(''),
+    email: new FormControl(''),
+    address: new FormGroup({
+      street: new FormControl(''),
+      suite: new FormControl(''),
+      city: new FormControl(''),
+      zipcode: new FormControl(''),
+      geo: new FormGroup({
+        lat: new FormControl(''),
+        lng: new FormControl(''),
+      }),
+    }),
+    phone: new FormControl(''),
+    website: new FormControl(''),
+    company: new FormGroup({
+      name: new FormControl(''),
+      catchPhrase: new FormControl(''),
+      bs: new FormControl(''),
+    }),
+  });
+
+  ngOnInit(): void {
+    this.form.patchValue({
+      id: this.user.id,
+      name: this.user.name,
+      username: this.user.username,
+      email: this.user.email,
+      phone: this.user.phone,
+      website: this.user.website,
+    });
+  }
 }
