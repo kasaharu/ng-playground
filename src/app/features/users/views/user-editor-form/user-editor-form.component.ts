69f5d4a4e860a0ebc20febae02da8a687f9b1f83
Wataru.Kasahara <kasahara1019@gmail.com>
2021-08-14 13:34:50 +0900

    feat(users): user 情報を form に渡す

--- src/app/features/users/views/user-editor-form/user-editor-form.component.ts
+++ src/app/features/users/views/user-editor-form/user-editor-form.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { User } from '../../../../domain/user';
 
 @Component({
   selector: 'app-user-editor-form',
@@ -7,6 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UserEditorFormComponent implements OnInit {
   constructor() {}
+  @Input() user: User | null = null;
 
   ngOnInit(): void {}
 }
--- src/app/features/users/views/user-list-editor/user-list-editor.component.ts
+++ src/app/features/users/views/user-list-editor/user-list-editor.component.ts
@@ -1,8 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { User } from '../../../../domain/user';
 
 @Component({
   selector: 'app-user-list-editor',
   templateUrl: './user-list-editor.component.html',
   styleUrls: ['./user-list-editor.component.scss'],
 })
-export class UserListEditorComponent {}
+export class UserListEditorComponent {
+  @Input() users: User[] | null = null;
+}
