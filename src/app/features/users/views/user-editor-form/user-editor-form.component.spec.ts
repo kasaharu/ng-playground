b032475540e690b794fa1f59184402f10ef9e2f3
Wataru.Kasahara <kasahara1019@gmail.com>
2021-08-15 18:08:35 +0900

    test(users): TestHostComponent を用意して Input プロパティを渡すようにした

--- src/app/features/users/views/user-editor-form/user-editor-form.component.spec.ts
+++ src/app/features/users/views/user-editor-form/user-editor-form.component.spec.ts
@@ -1,20 +1,32 @@
+import { Component } from '@angular/core';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { ReactiveFormsModule } from '@angular/forms';
+import { By } from '@angular/platform-browser';
+import { User } from '../../../../domain/user';
+import { createMockUser } from '../../../../testing/factories';
 import { UserEditorFormComponent } from './user-editor-form.component';
+
+@Component({
+  template: '<app-user-editor-form [user]="user"></app-user-editor-form>',
+})
+class TestHostComponent {
+  user: User = createMockUser({});
+}
 
 describe('UserEditorFormComponent', () => {
   let component: UserEditorFormComponent;
-  let fixture: ComponentFixture<UserEditorFormComponent>;
+  let fixture: ComponentFixture<TestHostComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [UserEditorFormComponent],
+      declarations: [UserEditorFormComponent, TestHostComponent],
+      imports: [ReactiveFormsModule],
     }).compileComponents();
   });
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(UserEditorFormComponent);
-    component = fixture.componentInstance;
+    fixture = TestBed.createComponent(TestHostComponent);
+    component = fixture.debugElement.query(By.directive(UserEditorFormComponent)).componentInstance;
     fixture.detectChanges();
   });
 
