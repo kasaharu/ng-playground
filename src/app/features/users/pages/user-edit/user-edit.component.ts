f7a8b51315b0bfcd892cfdd135dfad8c63652e83
Wataru.Kasahara <kasahara1019@gmail.com>
2021-09-03 20:29:07 +0900

    refactor(users): Page component に不要な selector を削除

--- src/app/features/users/pages/user-edit/user-edit.component.ts
+++ src/app/features/users/pages/user-edit/user-edit.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-user-edit',
   templateUrl: './user-edit.component.html',
   styleUrls: ['./user-edit.component.scss'],
 })
