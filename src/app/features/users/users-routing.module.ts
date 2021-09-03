a6a3c3c73c424c220c7e1df25d778dc914879faf
Wataru.Kasahara <kasahara1019@gmail.com>
2021-09-03 20:32:25 +0900

    feat(users): 編集画面に遷移するようにルーティングを設定

--- src/app/features/users/users-routing.module.ts
+++ src/app/features/users/users-routing.module.ts
@@ -1,8 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { UserEditPageComponent } from './pages/user-edit/user-edit.component';
 import { UsersPageComponent } from './pages/users/users.component';
 
-const routes: Routes = [{ path: '', component: UsersPageComponent, pathMatch: 'full' }];
+const routes: Routes = [
+  { path: '', component: UsersPageComponent, pathMatch: 'full' },
+  { path: ':userId/edit', component: UserEditPageComponent, pathMatch: 'full' },
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
