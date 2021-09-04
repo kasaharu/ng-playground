0fb834b58a2fa1af75ffce1975bf3462fc90b97f
Wataru.Kasahara <kasahara1019@gmail.com>
2021-09-04 16:59:49 +0900

    feat(users): 編集画面の状態を管理する store 機能を持った usecase class の実装

--- src/app/features/users/containers/user-edit/user-edit.usecase.ts
+++ src/app/features/users/containers/user-edit/user-edit.usecase.ts
@@ -1,8 +1,24 @@
 import { Injectable } from '@angular/core';
+import { ComponentStore } from '@ngrx/component-store';
+import { Observable } from 'rxjs';
+import { User } from '../../../../domain/user';
+import { UserApi } from '../../../../infrastructures/api/user.api';
 
-@Injectable({
-  providedIn: 'root',
-})
-export class UserEditUsecase {
-  constructor() {}
+export interface UserEditState {
+  user: User | null;
+}
+
+@Injectable()
+export class UserEditUsecase extends ComponentStore<UserEditState> {
+  constructor(private readonly _userApi: UserApi) {
+    super({ user: null });
+  }
+
+  readonly user$: Observable<User | null> = this.select((state) => state.user);
+  readonly saveUser = this.updater((state, user: User) => ({ ...state, user }));
+
+  async fetchUser(userId: number): Promise<void> {
+    const user = await this._userApi.getUser(userId).toPromise();
+    this.saveUser(user);
+  }
 }
