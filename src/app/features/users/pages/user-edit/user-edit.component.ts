d79500c8f0c724904f607a8fb32d6770b23c092b
Wataru.Kasahara <kasahara1019@gmail.com>
2021-09-04 17:26:12 +0900

    refactor(users): 不要な lifecycle method を削除

--- src/app/features/users/pages/user-edit/user-edit.component.ts
+++ src/app/features/users/pages/user-edit/user-edit.component.ts
@@ -1,11 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   templateUrl: './user-edit.component.html',
   styleUrls: ['./user-edit.component.scss'],
 })
-export class UserEditPageComponent implements OnInit {
-  constructor() {}
-
-  ngOnInit(): void {}
-}
+export class UserEditPageComponent {}
