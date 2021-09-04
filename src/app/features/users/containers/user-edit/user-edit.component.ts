9ff2c4cda5de92b1eb10a78313183f414398148e
Wataru.Kasahara <kasahara1019@gmail.com>
2021-09-04 17:02:55 +0900

    feat(users): 編集画面を開いたときに id 1 の user を取得

--- src/app/features/users/containers/user-edit/user-edit.component.ts
+++ src/app/features/users/containers/user-edit/user-edit.component.ts
@@ -1,12 +1,16 @@
 import { Component, OnInit } from '@angular/core';
+import { UserEditUsecase } from './user-edit.usecase';
 
 @Component({
   selector: 'app-user-edit',
   templateUrl: './user-edit.component.html',
   styleUrls: ['./user-edit.component.scss'],
+  providers: [UserEditUsecase],
 })
 export class UserEditComponent implements OnInit {
-  constructor() {}
+  constructor(private readonly _usecase: UserEditUsecase) {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this._usecase.fetchUser(1);
+  }
 }
