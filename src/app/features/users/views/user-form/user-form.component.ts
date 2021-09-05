5dd48b796be98996039a8bcffd1fcf2c661b230b
Wataru.Kasahara <kasahara1019@gmail.com>
2021-09-05 11:06:55 +0900

    feat(users): 編集画面用の FormGroup の定義

--- src/app/features/users/views/user-form/user-form.component.ts
+++ src/app/features/users/views/user-form/user-form.component.ts
@@ -1,8 +1,33 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup } from '@ngneat/reactive-forms';
+import { Address, Company, User } from '../../../../domain/user';
 
 @Component({
   selector: 'app-user-form',
   templateUrl: './user-form.component.html',
   styleUrls: ['./user-form.component.scss'],
 })
-export class UserFormComponent {}
+export class UserFormComponent {
+  addressForm = new FormGroup<Address>({
+    street: new FormControl(''),
+    suite: new FormControl(''),
+    city: new FormControl(''),
+    zipcode: new FormControl(''),
+    geo: new FormGroup({ lat: new FormControl(''), lng: new FormControl('') }),
+  });
+
+  userForm = new FormGroup<User>({
+    id: new FormControl(0),
+    name: new FormControl(''),
+    username: new FormControl(''),
+    email: new FormControl(''),
+    phone: new FormControl(''),
+    website: new FormControl(''),
+    company: new FormGroup<Company>({
+      name: new FormControl(''),
+      catchPhrase: new FormControl(''),
+      bs: new FormControl(''),
+    }),
+    address: this.addressForm,
+  });
+}
