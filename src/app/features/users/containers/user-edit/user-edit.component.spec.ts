c59bc57387f8eed0cba38632dc78a69f65616b86
Wataru.Kasahara <kasahara1019@gmail.com>
2021-09-13 08:36:10 +0900

    test: Fix console error when executing test

--- src/app/features/users/containers/user-edit/user-edit.component.spec.ts
+++ src/app/features/users/containers/user-edit/user-edit.component.spec.ts
@@ -1,3 +1,4 @@
+import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { UserEditComponent } from './user-edit.component';
 import { UserEditUsecase } from './user-edit.usecase';
@@ -13,6 +14,7 @@ describe('UserEditComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [UserEditComponent],
+      schemas: [CUSTOM_ELEMENTS_SCHEMA],
     })
       .overrideComponent(UserEditComponent, {
         add: { providers: [{ provide: UserEditUsecase, useClass: MockUserEditUsecase }] },
--- src/app/features/users/views/user-form/user-form.component.spec.ts
+++ src/app/features/users/views/user-form/user-form.component.spec.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ReactiveFormsModule } from '@angular/forms';
 import { By } from '@angular/platform-browser';
 import { createMockUser } from '../../../../testing/factories';
 import { UserFormComponent } from './user-form.component';
@@ -19,6 +20,7 @@ describe('UserFormComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [UserFormComponent, TestHostComponent],
+      imports: [ReactiveFormsModule],
     }).compileComponents();
   });
 
