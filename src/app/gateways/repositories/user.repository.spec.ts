c221bb7787c7a8bae3c80504406be0bbf59a3d9a
Wataru.Kasahara <kasahara1019@gmail.com>
2021-09-12 15:47:07 +0900

    refactor: Rename UserService -> UserRepository

---
+++ src/app/gateways/repositories/user.repository.spec.ts
@@ -0,0 +1,15 @@
+import { TestBed } from '@angular/core/testing';
+import { UserRepository } from './user.repository';
+
+describe('UserRepository', () => {
+  let repository: UserRepository;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({});
+    repository = TestBed.inject(UserRepository);
+  });
+
+  it('should be created', () => {
+    expect(repository).toBeTruthy();
+  });
+});
