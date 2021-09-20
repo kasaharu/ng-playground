5505b249bdbc276050352b57bfacd373ef0ef862
Wataru.Kasahara <kasahara1019@gmail.com>
2021-09-20 12:28:24 +0900

    feat(mock): mock data の用意と handler の実装

--- src/mocks/handlers.ts
+++ src/mocks/handlers.ts
@@ -1,7 +1,16 @@
 import { rest } from 'msw';
+import { findUser, updateUser, users } from './data/user';
 
 export const handlers = [
   rest.get('/users', (req, res, ctx) => {
-    return res(ctx.status(200), ctx.json([]));
+    return res(ctx.status(200), ctx.json(users));
+  }),
+  rest.get('/users/:userId', (req, res, ctx) => {
+    const userId = parseInt(req.params['userId']);
+    return res(ctx.json(findUser(userId)));
+  }),
+  rest.patch('/users/:userId', (req, res, ctx) => {
+    const userId = parseInt(req.params['userId']);
+    return res(ctx.json(updateUser(userId, req.body)));
   }),
 ];
