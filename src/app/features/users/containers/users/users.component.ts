5639744b72b9eea615dcfebe25176eadf6cd2e95
Wataru.Kasahara <kasahara1019@gmail.com>
2021-10-02 17:50:19 +0900

    refactor(users): 表示に関する部分を views の component に委譲

--- src/app/features/users/containers/users/users.component.ts
+++ src/app/features/users/containers/users/users.component.ts
@@ -11,16 +11,9 @@ import { UsersUsecase } from './users.usecase';
 export class UsersComponent implements OnInit {
   constructor(private readonly _usecase: UsersUsecase) {}
   readonly users$ = this._usecase.users$;
-  editable = false;
-  toggleButtonText = '編集モードへ';
 
   ngOnInit() {
     this._usecase.fetchUsers();
-  }
-
-  toggleMode(): void {
-    this.editable = !this.editable;
-    this.toggleButtonText = this.editable ? '編集モードを終了' : '編集モードへ';
   }
 
   updateUser(user: User): void {
--- src/app/features/users/views/ly-users/ly-users.component.ts
+++ src/app/features/users/views/ly-users/ly-users.component.ts
@@ -1,12 +1,24 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { User } from 'src/app/domain/user';
 
 @Component({
   selector: 'app-ly-users',
   templateUrl: './ly-users.component.html',
   styleUrls: ['./ly-users.component.scss'],
 })
-export class LyUsersComponent implements OnInit {
-  constructor() {}
+export class LyUsersComponent {
+  @Input() users: User[] = [];
+  @Output() userUpdated = new EventEmitter<User>();
 
-  ngOnInit(): void {}
+  editable = false;
+  toggleButtonText = '編集モードへ';
+
+  toggleMode(): void {
+    this.editable = !this.editable;
+    this.toggleButtonText = this.editable ? '編集モードを終了' : '編集モードへ';
+  }
+
+  updateUser(user: User): void {
+    this.userUpdated.emit(user);
+  }
 }
