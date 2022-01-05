94797408e4ccb5596b031c5f49a4d21a3c93f182
Wataru.Kasahara <kasahara1019@gmail.com>
2022-01-05 14:27:18 +0900

    feat(stamp): stamp を選択する UI の実装

--- src/app/features/stamp/pages/stamp/stamp.component.ts
+++ src/app/features/stamp/pages/stamp/stamp.component.ts
@@ -5,4 +5,17 @@ import { Component } from '@angular/core';
   templateUrl: './stamp.component.html',
   styleUrls: ['./stamp.component.scss'],
 })
-export class StampPageComponent {}
+export class StampPageComponent {
+  images = [
+    { id: 1, url: 'assets/animal_coelacanth.png' },
+    { id: 2, url: 'assets/kodai_elasmotherium.png' },
+    { id: 3, url: 'assets/kodai_same_helicoprion.png' },
+    { id: 4, url: 'assets/kodai_shisochou.png' },
+  ];
+  selectedImageId: number | null = null;
+  showStampField = true;
+
+  clickedImage(imageId: number) {
+    this.selectedImageId = imageId;
+  }
+}
