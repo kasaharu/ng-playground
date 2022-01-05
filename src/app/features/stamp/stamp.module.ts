faf034dfec94700c21eda7ce09fbf0346e918f17
Wataru.Kasahara <kasahara1019@gmail.com>
2022-01-05 10:13:53 +0900

    yarn ng g c features/stamp/pages/stamp

--- src/app/features/stamp/stamp.module.ts
+++ src/app/features/stamp/stamp.module.ts
@@ -2,10 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { StampRoutingModule } from './stamp-routing.module';
+import { StampComponent } from './pages/stamp/stamp.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    StampComponent
+  ],
   imports: [
     CommonModule,
     StampRoutingModule
