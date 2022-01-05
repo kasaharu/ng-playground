bfbddb51062d486012b29f9ec68b55aa2b3e481a
Wataru.Kasahara <kasahara1019@gmail.com>
2022-01-05 10:41:33 +0900

    feat(stamp): stamp へのルーティング設定

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,6 +5,7 @@ const routes: Routes = [
   { path: '', redirectTo: 'users', pathMatch: 'full' },
   { path: 'users', loadChildren: () => import('./features/users/users.module').then((m) => m.UsersModule) },
   { path: 'csv-parser', loadChildren: () => import('./features/csv-parser/csv-parser.module').then((m) => m.CsvParserModule) },
+  { path: 'stamp', loadChildren: () => import('./features/stamp/stamp.module').then((m) => m.StampModule) },
 ];
 
 @NgModule({
--- src/app/features/stamp/stamp-routing.module.ts
+++ src/app/features/stamp/stamp-routing.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { StampPageComponent } from './pages/stamp/stamp.component';
 
-const routes: Routes = [];
+const routes: Routes = [{ path: '', component: StampPageComponent, pathMatch: 'full' }];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class StampRoutingModule { }
+export class StampRoutingModule {}
