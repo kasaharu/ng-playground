9fff40343107bf844a61dd4cbc167fa8023decb9
Wataru.Kasahara <kasahara1019@gmail.com>
2022-01-05 10:11:33 +0900

    yarn ng g module features/stamp --routing

---
+++ src/app/features/stamp/stamp.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { StampRoutingModule } from './stamp-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    StampRoutingModule
+  ]
+})
+export class StampModule { }
