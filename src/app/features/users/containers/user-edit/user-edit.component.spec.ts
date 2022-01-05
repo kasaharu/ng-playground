fb6831f7c61400ae37a39a1bf28895a0c3808f55
Wataru.Kasahara <kasahara1019@gmail.com>
2022-01-05 14:54:43 +0900

    test(users): UserEditComponent の teardown を off にする

--- src/app/features/users/containers/user-edit/user-edit.component.spec.ts
+++ src/app/features/users/containers/user-edit/user-edit.component.spec.ts
@@ -1,25 +1,34 @@
 import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { BehaviorSubject } from 'rxjs';
+import { User } from '../../../../domain/user';
+import { createMockUser } from '../../../../testing/factories';
 import { UserEditComponent } from './user-edit.component';
 import { UserEditUsecase } from './user-edit.usecase';
 
 class MockUserEditUsecase {
+  user$ = new BehaviorSubject<User | null>(null);
   fetchUser(): void {}
 }
 
 describe('UserEditComponent', () => {
   let component: UserEditComponent;
   let fixture: ComponentFixture<UserEditComponent>;
+  let usecase: MockUserEditUsecase;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [UserEditComponent],
+      providers: [MockUserEditUsecase],
       schemas: [CUSTOM_ELEMENTS_SCHEMA],
+      teardown: { destroyAfterEach: false },
     })
       .overrideComponent(UserEditComponent, {
         add: { providers: [{ provide: UserEditUsecase, useClass: MockUserEditUsecase }] },
       })
       .compileComponents();
+
+    usecase = TestBed.inject(MockUserEditUsecase);
   });
 
   beforeEach(() => {
@@ -29,6 +38,7 @@ describe('UserEditComponent', () => {
   });
 
   it('should create', () => {
+    usecase.user$.next(createMockUser({}));
     expect(component).toBeTruthy();
   });
 });
